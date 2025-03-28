870056e94a8528afe9765634f3808149f27175b9
Your Name <you@example.com>
2025-03-28 22:44:00 +0200

    first commit

---
+++ app/components/Data.js
@@ -0,0 +1,90 @@
+"use client"
+import React, { useState } from "react";
+
+
+const Data = (posts) => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    console.log("posts: ", posts.posts);
+    
+
+  return (
+    <div className=" py-10">
+    {/* Gallery Section */}
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold text-center mb-6">Exhibition Gallery</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {/* {images.map((url, index) => (
+          <div
+            key={index}
+            className="overflow-hidden rounded-lg shadow-md cursor-pointer"
+            onClick={() => setSelectedImage(url)}
+          >
+            <img
+              src={url}
+              alt={`Exhibition Image ${index + 1}`}
+              className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+            />
+          </div>
+        ))} */}
+
+
+
+
+
+
+        {posts.posts.map((post) => {
+
+          return ( 
+            <div
+              key={post.sys.id}
+              className="overflow-hidden rounded-lg shadow-md cursor-pointer"
+              onClick={() => setSelectedImage(post.fields.img.fields.file.url)}
+            >
+              <img
+                src={post.fields.img.fields.file.url}
+                alt={`Exhibition Image ${post.sys.id + 1}`}
+                className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
+              />
+            </div>
+
+
+
+          );
+        })}
+
+
+
+
+
+      </div>
+    </div>
+
+    {/* Lightbox Modal */}
+    {selectedImage && (
+      <div
+        className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50"
+        onClick={() => setSelectedImage(null)} // Close modal on background click
+      >
+        <div className="relative">
+          {/* Close Button */}
+          <button
+            className="absolute top-4 right-4 text-white text-2xl font-bold"
+            onClick={() => setSelectedImage(null)}
+          >
+            &times;
+          </button>
+          {/* Enlarged Image */}
+          <img
+            src={selectedImage}
+            alt="Selected Exhibition"
+            className="max-w-full max-h-screen object-contain transition-transform duration-300"
+          />
+        </div>
+      </div>
+    )}
+  </div>
+  )
+}
+
+export default Data
--- app/exh/page.jsx
+++ app/exh/page.jsx
@@ -1,74 +1,29 @@
-"use client"
 
-import React, { useState } from "react";
-import  Footer  from "../components/Footer";
-import  Nav  from "../components/Nav";
 
-const ExhibitionGallery = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
 
-  const images = [
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866793/y08v8w1e4joyveaz2ewy.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866793/pc7nsc43dl5ugzmqvdpa.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866790/n6tsnhyh6bpqgzxzlaed.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866786/rrdwjbgibaptbsiegacz.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866785/ghymo5j8t5bwjzpnzb7t.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866779/obo9u3jmgggbkpv2aofo.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866775/bpcflqz1gunl1uuhn9ke.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866772/q2bof4rsbueg1qkzji65.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866771/z2lz7fmgnkdga5bqobrj.webp",
-    "https://res.cloudinary.com/dubdpmdwp/image/upload/v1732866767/q5hhxlw9bsxs7igocusc.webp",
-  ];
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+import Data from "../components/Data";
+import { client } from '../lib/contentful';
+
+
+const fetchPosts = async () => {
+  const response = await client.getEntries({ content_type: 'exh' });
+  return response.items;
+};
+
+const ExhibitionGallery = async () => {
+
+  const posts = await fetchPosts();
+
+ 
 
   return (
     <>
-    <Nav/>
-    <div className=" py-10">
-      {/* Gallery Section */}
-      <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold text-center mb-6">Exhibition Gallery</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {images.map((url, index) => (
-            <div
-              key={index}
-              className="overflow-hidden rounded-lg shadow-md cursor-pointer"
-              onClick={() => setSelectedImage(url)}
-            >
-              <img
-                src={url}
-                alt={`Exhibition Image ${index + 1}`}
-                className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300"
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+      <Nav />
+      <Data posts={posts} />
 
-      {/* Lightbox Modal */}
-      {selectedImage && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50"
-          onClick={() => setSelectedImage(null)} // Close modal on background click
-        >
-          <div className="relative">
-            {/* Close Button */}
-            <button
-              className="absolute top-4 right-4 text-white text-2xl font-bold"
-              onClick={() => setSelectedImage(null)}
-            >
-              &times;
-            </button>
-            {/* Enlarged Image */}
-            <img
-              src={selectedImage}
-              alt="Selected Exhibition"
-              className="max-w-full max-h-screen object-contain transition-transform duration-300"
-            />
-          </div>
-        </div>
-      )}
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
